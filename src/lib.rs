ebe392e55cc053c3a54aa95be12d2de4119e2282
Radu Popescu <mail@radupopescu.net>
2021-09-27 23:45:28 +0200

    Reorganise exports

--- src/lib.rs
+++ src/lib.rs
@@ -7,18 +7,13 @@
 
 mod errors;
 mod io_helpers;
-mod password_hash;
+pub mod password_hash;
 mod sign;
-mod srp;
+pub mod srp;
 mod symmetric;
 
 pub use {
-    crate::srp::{
-        generate_private_ephemeral, Client, ClientAfterHandshake, ClientProof, Mode, Server,
-        ServerProof, Verifier,
-    },
     errors::{Error, Result},
-    password_hash::{derive_key, gen_salt, Salt},
     sign::{KeyPair, PublicKey, Signature, SignatureStream},
     symmetric::{Key as SymmetricKey, Packet},
 };
